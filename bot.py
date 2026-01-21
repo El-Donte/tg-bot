acf75ae4b9dd4de5c3963e9bffbf61ce67275371
eldonte <nikitaabaturov49@gmail.com>
2026-01-21 20:16:45 +0500

    update

--- bot.py
+++ bot.py
@@ -6,18 +6,22 @@ from telegram.ext import Application, filters, CommandHandler, ContextTypes, Mes
 from telegram import Update
 import os
 
-from curl_cffi import requests
+import requests
 from bs4 import BeautifulSoup
 
 
 def get_response(url):
     headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,/;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, sdch', 'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-        'Cache-Control': 'max-age=0', 'Connection': 'close', 'Host': 'www.propertyguru.com.sg',
-        'Referer': 'propertyguru.com.sg/singapore-property-listing/…', 'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'ru',
+        'Cache-Control': 'max-age=0',
+        'Priority': 'u=0, i',
+        'Connection': 'close',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 YaBrowser/25.10.0.0 Safari/537.36'
     }
+
     scraper = cloudscraper.create_scraper(
         browser={
             'browser': 'chrome',
