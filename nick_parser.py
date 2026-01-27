a13bf9c3f8edb1a9d055d9c05c1420d940c118ec
eldonte <nikitaabaturov49@gmail.com>
2026-01-27 11:14:26 +0500

    update

--- nick_parser.py
+++ nick_parser.py
@@ -1,23 +1,10 @@
-import requests
+from curl_cffi import requests
 import cloudscraper
 from bs4 import BeautifulSoup
 
-import undetected_chromedriver as uc
+
 
 def get_response(url):
-    headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Encoding': 'gzip, deflate, br, zstd',
-        'Accept-Language': 'ru',
-        'Cache-Control': 'max-age=0',
-        'Host': 'ru.nickfinder.com',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/140.0.0.0 YaBrowser/25.10.0.0 Safari/537.36'
-    }
-
-    cookies = {
-        'PHPSESSID' : 'b38pvn1f7rdn0ooih5jq94ft56'
-    }
 
     scraper = cloudscraper.create_scraper(
         browser={
@@ -27,17 +14,36 @@ def get_response(url):
         }
     )
 
+    cookies = {
+        "cf_clearance": "твой_cf_clearance_сюда_______очень_длинный",
+        "__cf_bm": "тут_тоже_если_есть",
+        # другие куки, если они есть
+    }
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'ru,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+    }
+
+
 
 
     try:
-        # response = scraper.get(url, headers=headers, cookies=cookies)
-        # response.raise_for_status()
+        r = requests.get(url, headers=headers, cookies=cookies, timeout=15)
+        r.raise_for_status()
+    #     response = requests.get(url, impersonate="chrome", timeout=10)
+    #     response.raise_for_status()
 
-
-        driver = uc.Chrome()
-        page = driver.get('https://ru.nickfinder.com').text
-        return page
-    except requests.RequestException as e:
+        return r
+    except requests.RequestsError as e:
         print(f"Ошибка запроса: {e}")
 
 
