569f3cb615f32fc7f3d389b125afd361777d3259
eldonte <nikitaabaturov49@gmail.com>
2026-01-27 10:40:41 +0500

    update

--- nick_parser.py
+++ nick_parser.py
@@ -1,11 +1,10 @@
 import requests
 import cloudscraper
 from bs4 import BeautifulSoup
-from playwright.async_api import async_playwright
 
+import undetected_chromedriver as uc
 
-
-async def get_response(url):
+def get_response(url):
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Accept-Encoding': 'gzip, deflate, br, zstd',
@@ -34,25 +33,10 @@ async def get_response(url):
         # response = scraper.get(url, headers=headers, cookies=cookies)
         # response.raise_for_status()
 
-        async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=True)
-            context = await browser.new_context(
-                viewport={'width': 1920, 'height': 1080},
-                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
-                locale="ru-RU"
-            )
-            page = await context.new_page()
 
-            await page.goto("https://ru.nickfinder.com/", wait_until="networkidle", timeout=45000)
-
-            # ждём, пока Turnstile решится (обычно 3–15 сек)
-            content = await page.content()
-
-            # здесь можно уже парсить soup = BeautifulSoup(content, "lxml")
-
-            await browser.close()
-
-            return content
+        driver = uc.Chrome()
+        page = driver.get('https://ru.nickfinder.com').text
+        return page
     except requests.RequestException as e:
         print(f"Ошибка запроса: {e}")
 
@@ -68,9 +52,9 @@ def get_visible_text(soup):
     return "\n".join(lines)
 
 
-async def get_nicks_for_name(name):
+def get_nicks_for_name(name):
     url = f"https://ru.nickfinder.com/{name}"
-    response = await get_response(url)
+    response = get_response(url)
 
     soup = BeautifulSoup(response.text, "html.parser")
 
