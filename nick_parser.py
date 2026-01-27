78e48020d58d04e9a08230e494042d9eabe33a9e
eldonte <nikitaabaturov49@gmail.com>
2026-01-27 10:19:50 +0500

    update

--- nick_parser.py
+++ nick_parser.py
@@ -5,7 +5,7 @@ from playwright.async_api import async_playwright
 
 
 
-def get_response(url):
+async def get_response(url):
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Accept-Encoding': 'gzip, deflate, br, zstd',
@@ -34,23 +34,23 @@ def get_response(url):
         # response = scraper.get(url, headers=headers, cookies=cookies)
         # response.raise_for_status()
 
-        with async_playwright() as p:
-            browser = p.chromium.launch(headless=True)
-            context = browser.new_context(
+        async with async_playwright() as p:
+            browser = await p.chromium.launch(headless=True)
+            context = await browser.new_context(
                 viewport={'width': 1920, 'height': 1080},
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
                 locale="ru-RU"
             )
-            page = context.new_page()
+            page = await context.new_page()
 
-            page.goto("https://ru.nickfinder.com/", wait_until="networkidle", timeout=45000)
+            await page.goto("https://ru.nickfinder.com/", wait_until="networkidle", timeout=45000)
 
             # ждём, пока Turnstile решится (обычно 3–15 сек)
-            content = page.content()
+            content = await page.content()
 
             # здесь можно уже парсить soup = BeautifulSoup(content, "lxml")
 
-            browser.close()
+            await browser.close()
 
             return content
     except requests.RequestException as e:
